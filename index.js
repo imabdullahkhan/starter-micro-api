277e7cf759f2201b8d4516767f7045a8c7b57e22
Abdullah Khan <mabdullahkhan1996@gmail.com>
2024-01-16 02:58:33 +0500

    adding env support

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http');
+require('dotenv').config();
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!!!!');
+    res.write(`Yo!!!!${process.env.port}`);
     res.end();
 }).listen(process.env.PORT || 3000);
